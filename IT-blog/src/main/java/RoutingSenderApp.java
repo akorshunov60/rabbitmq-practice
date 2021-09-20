f2fa3fea58f5e345bde678aa0dc5100ab174eb2b
Alexander Korshunov <71963376+akorshunov60@users.noreply.github.com>
2021-09-20 15:52:14 +0300

    homeEx-8.1

--- IT-blog/src/main/java/RoutingSenderApp.java
+++ IT-blog/src/main/java/RoutingSenderApp.java
@@ -3,20 +3,26 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class RoutingSenderApp {
 
     private static final String EXCHANGE_NAME = "topic_exchange";
 
     public static void main(String[] argv) throws Exception {
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+
         try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+             Channel channel = connection.createChannel();) {
             channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
-
-            String routingKey = "prog.java.oop.inheritance";
-            String message = "message123";
-
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Введите тему, по которой хотите отправить сообщение, например java");
+            String routingKey = reader.readLine();
+            System.out.println("[Введите сообщение:]");
+            String message = reader.readLine();
             channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
         }
--- subscriber/src/main/java/RoutingReceiverApp.java
+++ subscriber/src/main/java/RoutingReceiverApp.java
@@ -1,29 +1,32 @@
 import com.rabbitmq.client.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class RoutingReceiverApp {
 
     private static final String EXCHANGE_NAME = "topic_exchange";
 
     public static void main(String[] argv) throws Exception {
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-
         channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
-
         String queueName = channel.queueDeclare().getQueue();
 
-        String routingKey = "prog.java.#";
-        channel.queueBind(queueName, EXCHANGE_NAME, routingKey);
-        System.out.println(" [*] Waiting for messages with routing key (" + routingKey + "):");
-
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
-
-        };
-        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
-        });
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите тему, которая вам интересна, например java");
+            String routingKeyJava = reader.readLine().replace("set_topic ", "");
+            channel.queueBind(queueName, EXCHANGE_NAME, routingKeyJava);
+            System.out.println(" [*] Waiting for messages");
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+            };
+            channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+            });
+        }
     }
 }
